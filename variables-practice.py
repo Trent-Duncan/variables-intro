917cd94fcc6ebda6dcbf49668ad0257c1524f2b3
Trent-Duncan <duncantr131@northwested.org>
2024-09-06 13:53:54 -0400

    Update variables-practice.py

--- variables-practice.py
+++ variables-practice.py
@@ -19,7 +19,7 @@
 # Define a variable called greeting and assign it a string value you'd use to greet someone (Good morning, Good afternoon, etc.)
 # Use the print function to print the value of one of three variables you created in Part 1
 school = 'Career Tech'
-favorite_superhero = 'IronMan'
+favorite_superhero = "IronMan"
 
 
 
